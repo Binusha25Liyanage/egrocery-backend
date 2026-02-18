7108154bfebcc60c351a6994d8b4e46c88635be6
Binusha Liyanage <binusha45liyanage@gmail.com>
2026-02-18 10:51:33 +0530

    feat: Implement JWT security with `JwtService` and `SecurityConfig`.

--- src/main/java/com/egrocery_remasterd/egrocery_backend/dto/AuthResponse.java
+++ src/main/java/com/egrocery_remasterd/egrocery_backend/dto/AuthResponse.java
@@ -2,9 +2,11 @@ package com.egrocery_remasterd.egrocery_backend.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class AuthResponse {
     private String token;
 }
