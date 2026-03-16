a5554f12e48e322ec09aa90375f1f7b41314d677
Binusha Liyanage <binusha45liyanage@gmail.com>
2026-03-16 15:19:20 +0530

    feat: Implement JWT authentication by adding security configuration and a JWT filter.

--- src/main/java/com/egrocery_remasterd/egrocery_backend/config/SecurityConfig.java
+++ src/main/java/com/egrocery_remasterd/egrocery_backend/config/SecurityConfig.java
@@ -1,15 +1,49 @@
 package com.egrocery_remasterd.egrocery_backend.config;
 
+import com.egrocery_remasterd.egrocery_backend.security.JwtAuthenticationFilter;
+import com.egrocery_remasterd.egrocery_backend.security.JwtService;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
 
+    private final JwtService jwtService;
+
+    public SecurityConfig(JwtService jwtService) {
+        this.jwtService = jwtService;
+    }
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public JwtAuthenticationFilter jwtAuthenticationFilter() {
+        return new JwtAuthenticationFilter(jwtService);
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/auth/**").permitAll()
+                        .anyRequest().authenticated())
+                .addFilterBefore(
+                        jwtAuthenticationFilter(),
+                        UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
     }
 }
